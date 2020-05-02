790c3dac0cc1d10600a0cb85707c206a4e9f4ef9
Lilian VERLHAC <lilian.verlhac@epitech.eu>
2020-05-03 00:06:52 +0200

    [ADD] reception class and unit tests + lowercase for pizza name

---
+++ src/Reception.cpp
@@ -0,0 +1,144 @@
+/*
+** EPITECH PROJECT, 2020
+** plazza
+** File description:
+** Reception implementation
+*/
+
+#include "Reception.hpp"
+#include "Marmiton.hpp"
+#include "exception.hpp"
+#include "shd/WordIterator.hpp"
+#include <iostream>
+#include <iterator>
+
+namespace plazza
+{
+Reception::Reception(int ac, char const* const* av)
+{
+    using namespace std::chrono_literals;
+
+    size_t pos = 0;
+
+    if (ac < 4)
+    {
+        throw ArgumentException("Bad number of argument.");
+    }
+
+    std::string smultiplier = av[1];
+    std::string snbr_cook = av[2];
+    std::string srestock_time = av[3];
+
+    this->multiplier = std::stof(smultiplier, &pos);
+    if (pos != smultiplier.size())
+    {
+        throw ArgumentException("Invalid multiplier. Should be float");
+    }
+
+    this->nbr_cook = std::stoi(snbr_cook, &pos);
+    if (pos != snbr_cook.size() or this->nbr_cook < 1)
+    {
+        throw ArgumentException("Invalid cook number."
+                                "Should be positive non-null integer.");
+    }
+
+    this->restock_time =
+        std::chrono::milliseconds{std::stoul(srestock_time, &pos)};
+    if (pos != srestock_time.size() or this->restock_time < 1ms)
+    {
+        throw ArgumentException("Invalid restock couldown."
+                                "Should be non-nul positive.");
+    }
+}
+
+auto Reception::strToPizzasize(std::string_view str) -> pizzas::size
+{
+    auto dict_str_pizzas =
+        std::array<std::pair<std::string_view, pizzas::size>, 5>{
+            {{"S", pizzas::size::S},
+             {"M", pizzas::size::M},
+             {"L", pizzas::size::L},
+             {"XL", pizzas::size::XL},
+             {"XXL", pizzas::size::XXL}}};
+    auto const* it =
+        std::find_if(dict_str_pizzas.begin(), dict_str_pizzas.end(),
+                     [str](auto pair_str_pizza) {
+                         return (str == pair_str_pizza.first);
+                     });
+
+    if (it != dict_str_pizzas.end())
+    {
+        return it->second;
+    }
+    throw DeserializingException("Fail to get size in str.");
+}
+
+auto Reception::parseOrder(std::string const& sorders) -> std::vector<Order>
+{
+    shd::WordIterator<';'> order_selector{sorders};
+    std::vector<Order> orders;
+
+    for (auto order: order_selector)
+    {
+        shd::WordIterator<' '> order_attributes{order};
+
+        try
+        {
+            // refacto later
+            // std::distance unimplemented in word iterator
+            if (order_attributes != order_attributes.end())
+            {
+                auto const& recipe = *order_attributes >> marmiton;
+                ++order_attributes;
+
+                if (order_attributes != order_attributes.end())
+                {
+                    auto pizza_size = strToPizzasize(*order_attributes);
+                    ++order_attributes;
+
+                    if (order_attributes != order_attributes.end())
+                    {
+                        size_t nbr_pizza = 0;
+
+                        if ((*order_attributes)[0] == 'x')
+                        {
+                            nbr_pizza = std::stoul(
+                                std::string(*order_attributes).substr(1));
+
+                            ++order_attributes;
+                        }
+                        if (order_attributes == order_attributes.end())
+                        {
+                            std::vector<Order> inserted{
+                                nbr_pizza, Order{recipe, pizza_size}};
+
+                            orders.insert(
+                                orders.end(),
+                                std::make_move_iterator(inserted.begin()),
+                                std::make_move_iterator(inserted.end()));
+                        }
+                    }
+                }
+            }
+        } catch (std::exception const& e)
+        {}
+    }
+    return orders;
+}
+
+auto Reception::getMultiplier() const -> float const&
+{
+    return this->multiplier;
+}
+
+auto Reception::getCookNbr() const -> unsigned int const&
+{
+    return this->nbr_cook;
+}
+
+auto Reception::getRestockTime() const -> std::chrono::milliseconds const&
+{
+    return this->restock_time;
+}
+
+}
---
+++ tests/reception_tests.cpp
@@ -0,0 +1,73 @@
+/*
+** EPITECH PROJECT, 2020
+** plazza
+** File description:
+** reception unit tests
+*/
+
+#include "Reception.hpp"
+#include "include/catch2.hpp"
+#include <chrono>
+
+TEST_CASE("Reception arg parser", "Arg parser")
+{
+    SECTION("well formated input")
+    {
+        constexpr auto av = std::array<char const*, 4>{"./plazza", "2", "5", "2000"};
+        CHECK_NOTHROW(plazza::Reception(4, av.data()));
+    }
+
+    SECTION("Fucked up input")
+    {
+        constexpr auto av = std::array<char const*, 4>{"./plaaazza", "aaaa2", "aaa5", "200aaaa0"};
+        CHECK_THROWS(plazza::Reception(4, av.data()));
+    }
+
+    SECTION("Not enough input")
+    {
+        constexpr auto av = std::array<char const*, 1>{"./plaaazza"};
+        CHECK_THROWS(plazza::Reception(1, av.data()));
+    }
+    SECTION("Arguments")
+    {
+        using namespace std::chrono_literals;
+
+        constexpr auto av = std::array<char const*, 4>{"./plazza", "2", "5", "2000"};
+        auto rec = plazza::Reception(4, av.data());
+
+        CHECK(rec.getMultiplier() == 2);
+        CHECK(rec.getCookNbr() == 5);
+        CHECK(rec.getRestockTime() == 2000ms);
+    }
+}
+
+TEST_CASE("Reception input parser", "input parser")
+{
+    SECTION("well formated input")
+    {
+        auto za = plazza::Reception::parseOrder("regina XXL x2; fantasia M x3; margarita S x1");
+
+        CHECK(za.size() == 6);
+        CHECK(za[1].size == plazza::pizzas::size::XXL);
+        CHECK(za[1].recipe.get().getName() == "regina");
+    }
+
+    SECTION("Fucked up input")
+    {
+
+        auto za = plazza::Reception::parseOrder("regioina XaXL x2; fantasia M x; margarita S x1");
+
+        CHECK(za.size() == 1);
+        CHECK(za[0].size == plazza::pizzas::size::S);
+        CHECK(za[0].recipe.get().getName() == "margarita");
+    }
+    SECTION("Good middle pizza")
+    {
+
+        auto za = plazza::Reception::parseOrder("regioina XaXL x2; fantasia M x1; margarita S xk");
+
+        CHECK(za.size() == 1);
+        CHECK(za[0].size == plazza::pizzas::size::M);
+        CHECK(za[0].recipe.get().getName() == "fantasia");
+    }
+}
